a02a8064fb12e475a9f7f907442c0e80a3de83d0
.-.. .- . .-. - . <laertelai12@gmail.com>
2026-04-29 15:22:35 -0300

    Otimização do código
    Importei a Fonte, Header e Sidebar para a pagina de Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
-import App from "next/app";
-import Image from "next/image";
-import Header from "@/components/header";
-import Sidebar from "@/components/sidebar";
-
 export default function Home() {
   return (
   <div>
-    <Header />
-    <Sidebar />
       <main>
         <h1>Sistema de Ponto</h1>
       </main>
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,33 +1,21 @@
 export default function Sidebar() {
-  return (
-<aside className="barraLateral">
-        
-        <nav>
-            <h1>Sistema de Ponto</h1>
+    return (
+        <aside className="barraLateral">
+            <nav>
+                <h1>Sistema de Ponto</h1>
 
-            <ul>
-                <div className="sessao-inicio">
-                    <li>
-                        <a href="#"><img src="/inicio-icon.svg" alt="" className="img-home" />Inicio</a>
+                <ul>
+                    <li className="sessao-inicio">
+                        <a href="/teste.tsx"><img src="/inicio-icon.svg" alt="Início" /> Início</a>
                     </li>
-                </div>
-
-                <div className="sessao-movimentacao">
-                    <li>
-                        <a href=""><img src="/movimentacao-icon.svg" alt="" className="img-movimentacao"/> Movimentações</a>
+                    <li className="sessao-movimentacao">
+                        <a href="#"><img src="/movimentacao-icon.svg" alt="Movimentações" /> Movimentações</a>
                     </li>
-                </div>
-
-                <div className="sessao-configuracao">
-                    <li>
-                        <a href=""><img src="/configuracoes-icon.svg" alt="" className="img-config"/>Configurações</a>
-                        <ul>
-                        </ul>
+                    <li className="sessao-configuracao">
+                        <a href="#"><img src="/configuracoes-icon.svg" alt="Configurações" /> Configurações</a>
                     </li>
-                </div>
-            </ul>
-        </nav>
-    </aside>
-
-  );
+                </ul>
+            </nav>
+        </aside>
+    );
 } 
